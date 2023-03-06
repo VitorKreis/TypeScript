5a57de13bb507eb6f636a5190053404e74333ccb
VitorKreis <101025640+VitorKreis@users.noreply.github.com>
2023-03-06 20:09:59 -0300

    Configurando tudo para o typescript

---
+++ Basico/.prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    semi: true,
+    singleQuote: true,
+    trailingComma: 'all',
+    'prettier/prettier': [
+      'error',
+      {
+        'endOfLine': 'auto',
+      }
+    ]
+  }
+  
